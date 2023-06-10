451ea8c75990b566596472089f4739c79657c0d1
Vincent Phung <vincent.j.phung@gmail.com>
2023-06-10 09:28:30 -0700

    added aos effect

--- script.js
+++ script.js
@@ -1,71 +1,8 @@
-// AOS.init({
-//   duration: 1000,
-//   delay: 500,
-//   easing: 'ease-in-out',
-// });
-
-// window.addEventListener("DOMContentLoaded", function () {
-//   const menuIcon = document.querySelector("#menu-icon");
-//   const navbar = document.querySelector(".navbar");
-
-//   menuIcon.addEventListener("click", () => {
-//     menuIcon.classList.toggle("bx-x");
-//     navbar.classList.toggle("active");
-//   });
-
-//   const sections = document.querySelectorAll("section");
-//   const navLinks = document.querySelectorAll('header nav a[href^="#"]');
-//   const header = document.querySelector("header");
-
-//   window.addEventListener("scroll", () => {
-//     const top = window.scrollY;
-
-//     sections.forEach((sec) => {
-//       const height = sec.offsetHeight;
-//       const offset = sec.offsetTop - header.offsetHeight - 150;
-//       const id = sec.getAttribute("id");
-
-//       if (top >= offset && top < offset + height) {
-//         const prevActiveLink = document.querySelector("header nav a.active");
-//         const currActiveLink = document.querySelector(`header nav a[href="#${id}"]`);
-
-//         if (prevActiveLink !== currActiveLink) {
-//           prevActiveLink?.classList.remove("active");
-//           currActiveLink?.classList.add("active");
-//         }
-//       }
-//     });
-//   });
-
-//   navLinks.forEach((link) => {
-//     link.addEventListener("click", () => {
-//       menuIcon.classList.remove("bx-x");
-//       navbar.classList.remove("active");
-//     });
-//   });
-
-//   const typed = new Typed(".multiple-text", {
-//     strings: ["Frontend Developer", "Software Engineer", "UI/UX"],
-//     typeSpeed: 80,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-//   });
-
-//   const typedAbout = new Typed(".about-text", {
-//     strings: ["Software Engineer", "UI/UX", "Frontend Developer", "Doggie Lover"],
-//     typeSpeed: 50,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-//   });
-// });
-
-
 AOS.init({
   duration: 1000,
   delay: 500,
   easing: 'ease-in-out',
+  offset: 120,  // Change this value to tweak when animation starts
 });
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -116,29 +53,18 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
   const typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Software Engineer", "UI/UX", "Versatile", "Detail-Oriented", "Agile", "Problem-solving Mindset", "Strong Collaboration Skills"],
+   strings: ["Frontend Developer", "Software Engineer", "Full Stack Developer", "Versatile", "Detail-Oriented", "Experienced with Agile", "Problem-Solver", "Collaborative Team Member", "Proactive Leader", "Efficient Project Manager", "Strong Leadership Skills", "Excellent Communicator"],
     typeSpeed: 50,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
   });
+
+  const typedAbout = new Typed(".about-text", {
+  strings: ["Passionate about Tech", "Innovative Thinker", "Collaborative Partner", "Results-driven Professional", "Dedicated and Committed", "Adaptable to Change", "Team Player", "Critical Thinker", "Reliable Colleague", "Fast Learner", "Skilled Negotiator", "Effective Problem Solver"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
   });
-
-//   const typedAbout = new Typed(".about-text", {
-//     strings: ["Software Engineer", "UI/UX", "Frontend Developer", "Doggie Lover"],
-//     typeSpeed: 50,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-//   });
-// });
-
-
-const typedAbout = new Typed(".about-text", {
- strings: ["Passionate", "Innovative", "Collaborative", "Results-driven", "Dedicated", "Adaptable", "Team Player"],
-  typeSpeed: 50,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true
 });
-
